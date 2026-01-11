6f0daa3d94f6438df32840f95ccfd01df2b934c2
Bo Kovach <bo.kovachh@gmail.com>
2026-01-11 20:22:48 +0200

    Add register_user, upload_vehicle, allow_route, make_trip, repair_vehicles and users_report metod in managinf_app file

--- project/main.py
+++ project/main.py
@@ -1,14 +1,26 @@
 from project.managing_app import ManagingApp
-from project.user import User
 
-# user = User('Tong', 'B0', '1930')
-
-user_manage = ManagingApp()
-
-print(user_manage.register_user('Tong', 'Bo', '1234'))
-print(user_manage.register_user('Jonh', 'Jones', '1234'))
-
-print(user_manage.upload_vehicle('PassengerCar', 'Jiguli', '1200', '1930'))
-print(user_manage.upload_vehicle('PassengerCar', 'Chrysler', '200C', '1930'))
-print(user_manage.upload_vehicle('PassengerCar', 'Chrysler', '200C', '1730'))
-
+app = ManagingApp()
+print(app.register_user( 'Tisha', 'Reenie', '7246506' ))
+print(app.register_user( 'Bernard', 'Remy', 'CDYHVSR68661'))
+print(app.register_user( 'Mack', 'Cindi', '7246506'))
+print(app.upload_vehicle('PassengerCar', 'Chevrolet', 'Volt', 'CWP8032'))
+print(app.upload_vehicle( 'PassengerCar', 'Volkswagen', 'e-Up!', 'COUN199728'))
+print(app.upload_vehicle('PassengerCar', 'Mercedes-Benz', 'EQS', '5UNM315'))
+print(app.upload_vehicle('CargoVan', 'Ford', 'e-Transit', '726QOA'))
+print(app.upload_vehicle('CargoVan', 'BrightDrop', 'Zevo400', 'SC39690'))
+print(app.upload_vehicle('EcoTruck', 'Mercedes-Benz', 'eActros', 'SC39690'))
+print(app.upload_vehicle('PassengerCar', 'Tesla', 'CyberTruck', '726QOA'))
+print(app.allow_route('SOF', 'PLD', 144))
+print(app.allow_route('BUR', 'VAR', 87))
+print(app.allow_route('BUR', 'VAR', 87))
+print(app.allow_route('SOF', 'PLD', 184))
+print(app.allow_route('BUR', 'VAR', 86.999))
+print(app.make_trip('CDYHVSR68661', '5UNM315', 3, False))
+print(app.make_trip('7246506', 'CWP8032', 1, True))
+print(app.make_trip('7246506', 'COUN199728', 1, False))
+print(app.make_trip('CDYHVSR68661', 'CWP8032', 3, False))
+print(app.make_trip('CDYHVSR68661', '5UNM315', 2, False))
+print(app.repair_vehicles(2))
+print(app.repair_vehicles(20))
+print(app.users_report())
--- project/managing_app.py
+++ project/managing_app.py
@@ -1,3 +1,4 @@
+from project.route import Route
 from project.user import User
 from project.vehicles.cargo_van import CargoVan
 from project.vehicles.passenger_car import PassengerCar
@@ -34,41 +35,99 @@ class ManagingApp:
         return f"{brand} {model} was successfully uploaded with LPN-{license_plate_number}."
 
     def allow_route(self, start_point: str, end_point: str, length: float):
-        route = self.find_route(start_point, end_point, length)
-        if route:
-            return f"{start_point}/{end_point} - {length} {'km' if route.length == length else 'shorter route'}" \
-                   f" had already been added to our platform."
+        route_id = max([r.route_id for r in self.routes], default=0) + 1
+        same_routes = self.find_same_routes(start_point, end_point)
 
-        route.route_id = 1 if route.route_id is None else + 1
-        self.routes.append(route)
+        if same_routes and any(route.length == length for route in same_routes):
+            return f"{start_point}/{end_point} - {length} km had already been added to our platform."
+
+        if same_routes and any(route.length < length for route in same_routes):
+            return f"{start_point}/{end_point} shorter route had already been added to our platform."
+
+        if same_routes:
+            self.lock_route(same_routes, length)
+
+        new_route = Route(start_point, end_point, length, route_id)
+        self.routes.append(new_route)
         return f"{start_point}/{end_point} - {length} km is unlocked and available to use."
 
 
     def make_trip(self, driving_license_number: str, license_plate_number: str, route_id: int,  is_accident_happened: bool):
-        pass
+        user = self.find_user_by_license(driving_license_number)
+        if user.is_blocked:
+            return f"User {driving_license_number} is blocked in the platform! This trip is not allowed."
+
+        vehicle = self.find_vehicle_by_license(license_plate_number)
+        if vehicle.is_damaged:
+            return f"Vehicle {license_plate_number} is damaged! This trip is not allowed."
+
+        route = self.find_route_by_id(route_id)
+        if route.is_locked:
+            return f"Route {route_id} is locked! This trip is not allowed."
+
+        vehicle.drive(route.length)
+
+        if is_accident_happened:
+            vehicle.change_status()
+            user.decrease_rating()
+        else:
+            user.increase_rating()
+
+        return f"{vehicle.brand} {vehicle.model} License plate: {license_plate_number}" \
+               f" Battery: {int(vehicle.battery_level)}% Status: {vehicle.status}"
 
     def repair_vehicles(self, count: int):
-        pass
+        damaged_vehicles = self.find_damaged_vehicles(count)
+        sorted_vehicles = sorted(damaged_vehicles, key=lambda x: (x.brand, x.model))
+
+        for vehicle in sorted_vehicles:
+            vehicle.is_damaged = False
+            vehicle.recharge()
+
+        return f"{len(sorted_vehicles)} vehicles were successfully repaired!"
 
     def users_report(self):
-        pass
+        result = ["*** E-Drive-Rent ***"]
+        sorted_users = sorted(self.users, key=lambda u: u.rating, reverse=True)
+
+        for user in sorted_users:
+            result.append(user.__str__())
+
+        return '\n'.join(result).strip()
+
+
 
     def find_user_by_license(self, driving_license_number):
-        for user in self.users:
-            if user.driving_license_number == driving_license_number:
-                return user
-
+        return next((u for u in self.users if u.driving_license_number == driving_license_number), None)
 
     def find_vehicle_by_license(self, license_plate_number):
-        for vehicle in self.vehicles:
-            if vehicle.license_plate_number == license_plate_number:
-                return vehicle
+        return next((v for v in self.vehicles if v.license_plate_number == license_plate_number), None)
 
+    def find_route_by_id(self, route_id):
+        return next((r for r in self.routes if r.route_id == route_id), None)
 
-    def find_route(self, start_point, end_point, length):
+    def find_same_routes(self, start_point, end_point):
+        routes = []
         for route in self.routes:
-            if route.start_point == start_point and route.end_point == end_point and route.length <= length:
-                return route
+            if route.start_point == start_point and route.end_point == end_point:
+                routes.append(route)
+
+        if routes:
+            return routes
+        return []
+
+    @staticmethod
+    def lock_route(same_routes, length):
+        for route in same_routes:
+            if route.length > length:
+                route.is_locked = True
+
+    def find_damaged_vehicles(self, count):
+        vehicles = [vehicle for vehicle in self.vehicles if vehicle.is_damaged][:count]
+        if vehicles:
+            return vehicles
+        return []
+
 
 
 
